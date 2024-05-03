aa35b78594e2fb5d66cef235e6acc69ed63ddbbf
Yusuf Eken <168830445+Yusuf-Eken@users.noreply.github.com>
2024-05-03 16:11:27 +0300

    Add files via upload

---
+++ Ders Notlarım/Hafta4/Hafta4/Program.cs
@@ -0,0 +1,234 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hafta4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+                //for(int i = 0; i <= 100; i++) {
+                //    Console.WriteLine(i);
+                //}
+
+                //int a = 0;
+                //while(a<=100) {
+                //    Console.WriteLine(a);
+                //    a++;
+                //}
+
+                //int b= 0;
+                //do
+                //{
+                //    Console.WriteLine(b);
+                //      b++;
+                //} while (b<=100);
+
+                /*1 den 10 a kadar olan sayıların toplamı
+
+                int top = 0;
+                for(int i = 0;i < 10;i++) {
+                    Console.WriteLine(i);
+                    top += i;
+                }
+                Console.WriteLine(top);
+                */
+
+
+                /*1 den kullanıcıdan alınan sayıya kadar olan sayıları toplayan alg.
+
+                Console.WriteLine("Bir sayi giriniz:");
+                int sayi,top=0,a=0;
+                sayi=Convert.ToInt32(Console.ReadLine());
+                while(a<=sayi) {
+                    Console.WriteLine(sayi);
+
+                    top += a;
+                    a++;
+                }
+                Console.WriteLine(top);
+                */
+                /*
+
+                Console.WriteLine("1 ile 100 arasındaki çift sayılar");
+                for (int i=0;i<100;i++)
+                {
+                    if(i%2==0)
+                    {
+                        Console.WriteLine(i);
+                    }
+                }
+
+                Console.WriteLine("1 ile 100 arasındaki tek sayılar");
+                for (int a = 0; a < 100; a++)
+                {
+                    if (a % 2 == 1)
+                    {
+                        Console.WriteLine(a);
+                    }
+                }
+                */
+
+                //10 ve 20 arasındaki tek sayıları toplayınız ve toplam sonucunu yazdırınız.
+
+                //int top = 0;
+                //for (int c = 10; c < 20; c++)
+                //{
+                //    if (c % 2 == 1)
+                //    {
+                //        top += c;
+                //    }
+
+                //}
+                //Console.WriteLine(top);
+
+
+                //100 den 1 e kadar 
+
+                //for (int i = 100; i >= 1; i--)
+                //{
+                //    Console.Write(" " + i);
+                //}
+
+                //1 50 arasında 3 ün katı olan sayıları yazdırınız.
+
+                //for(int i = 1; i < 50; i++) {
+                //    if (i % 3 == 0)
+                //    {
+                //        Console.WriteLine(i);
+                //    }
+                //}
+
+                //1 50 arasında 5 ün katı olan sayıları yazdırınız.
+
+                //for (int i = 1; i < 50; i++)
+                //{
+                //    if (i % 5 == 0)
+                //    {
+                //        Console.WriteLine(i);
+                //    }
+                //}
+
+                //Kullanıcıdan alınan sayının pozitif tam bölenlerini yazdırınız
+
+                //int sayi;
+                //Console.WriteLine("Sayı giriniz");
+                //sayi = Convert.ToInt32(Console.ReadLine());
+                //for (int i = 1; i <= sayi; i++)
+                //{
+
+                //    if (sayi % i == 0)
+                //    {
+
+                //        Console.WriteLine(i);
+                //    }
+
+                // 1 le 100 arasında çift sayıların kaç tane olduğunu görün
+
+                //int sayac=0;
+                //for(int i = 1; i <= 100; i++) {
+                //    if (i % 2 == 0)
+                //    {
+                //        Console.Write(i+" ");
+                //        sayac++;
+                //    }
+                //}
+                //Console.WriteLine("\n");
+                //Console.WriteLine("\n1 ile 100 arasında olan çift sayıların adeti:"+sayac);
+
+                // 1 le 100 arasında 3 e bölünen sayıların kaç tane olduğunu görün
+
+                //int sayac = 0;
+                //for (int i = 1; i <= 100; i++)
+                //{
+                //    if (i % 3 == 0)
+                //    {
+                //        Console.Write(i + " ");
+                //        sayac++;
+                //    }
+                //}
+                //Console.WriteLine("\n");
+                //Console.WriteLine("\n1 ile 100 arasında olan çift sayıların adeti:" + sayac);
+
+                //10 satır olarak 1 den 5 e kadar sayıları yazmak için 
+
+                //for (int i = 1; i <= 10; i++) {
+                //    for (int j = 1; j <= 5; j++) {
+                //        Console.Write(j+" ");
+                //    }
+                //    Console.WriteLine();
+                //}
+
+                //5x5 yıldız 
+
+                //for (int i = 1; i <=5; i++) 
+                //{
+                //    for(int j = 1; j <=5; j++)
+                //    {
+                //        Console.Write("*");
+                //    }
+                //    Console.WriteLine();
+                //}
+
+                //1 ler çarpım tablosu
+
+                //for(int i=1;i<=10;i++) 
+                //{
+                //    Console.WriteLine(1+"*"+i+"="+(i*1));
+                //}
+
+                //Çarpım tablosu
+
+                //for (int i = 1; i <= 10; i++) 
+                //{
+                //    for (int j = 1; j <= 10; j++)
+                //    {
+                //        Console.WriteLine(i+"*"+j+"="+i*j);
+                //    }
+                //    Console.WriteLine();
+                //}
+
+                //Dışarıdan girilen sayının birler onlar yüzler basamaığını bulunuz ve basamak değerlerini toplayınız.
+                /*
+                int sayi, onlarBas, yuzlerBas, on, yuz, top;
+                float birlerBas;
+                Console.WriteLine("Sayi giriniz:");
+
+                sayi = Convert.ToInt32(Console.ReadLine());
+
+                birlerBas = sayi % 10;
+                onlarBas = sayi % 100;
+                yuzlerBas = sayi % 1000;
+                on = onlarBas / 10;
+                yuz = yuzlerBas / 100;
+                top = on + yuz + (int)birlerBas;
+
+                Console.WriteLine("Sayinin birler basamağı:" + birlerBas);
+                Console.WriteLine("Sayinin onlar basamağı:" + on);
+                Console.WriteLine("Sayinin yüzler basamağı:" + yuz);
+                Console.WriteLine("Basamakların toplamı:" + top);
+                */
+                //kolay yolu
+                /*
+                int basamak;
+                int t = 0;
+                int sayi = 0;
+                do
+                {
+                basamak = sayi % 10;
+                t += basamak;
+                Console.WriteLine(basamak);
+                sayi = sayi / 10;
+                } while (sayi > 0);
+                Console.ReadLine;
+                */
+            }
+                
+        }
+                
+    }
+
+
---
+++ Ders Notlarım/Hafta7/Hafta7/Program.cs
@@ -0,0 +1,179 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Emit;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hafta7
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //GEÇEN SENENİN VİZE SORULARI
+
+            //1
+
+            /*
+            int n;
+            double coz=0;
+            Console.WriteLine("N ssayisini giriniz:");
+            n = Convert.ToInt32(Console.ReadLine());
+            for (int i = 1; i < n; i++)
+            {
+                    coz+=(double)i / (i + 1.0);
+            }
+            Console.WriteLine(coz);
+            */
+
+            //2
+
+            /*
+            int n;
+            double coz = 0;
+            int pay = 1;
+            Console.WriteLine("N sayisini giriniz:");
+            n=Convert.ToInt32(Console.ReadLine());
+            for(int i = 1; i <= n; i++) {
+                coz += pay / (double)i;
+            }
+            Console.WriteLine(coz);
+            */
+
+            //3- 9 elemanlı bir sayı dizisi tanımlayınız.dizinin elemanlarını kullanıcan
+            //alarak diziyi oluşturunuz.yan yana yazdırınız ilk
+            //ve sonuncu elemanlarını toplayınız.
+
+            /*
+            int[] dizi = new int [9];
+            int top;
+            for (int i = 0; i < dizi.Length; i++)
+            {
+                Console.WriteLine(i+1+". elemanı giriniz:");
+                dizi[i]=int.Parse(Console.ReadLine());
+            }
+            for (int i = 0; i < dizi.Length; i++)
+            {
+                Console.WriteLine(dizi[i]+" ");
+            }
+            top = dizi[0] + dizi[8] + dizi[4];
+            Console.WriteLine("ilk son ve orta elemanlarının toplamı:"+top);
+            */
+
+            //4- 10 elemanlı bir dizi tanımalyınız elemanları 1-9 dahil rastgele oluşturunuz diziyi yazdırınız elemanlarını %10 arttırınız yeni diziyi yazdırınız.
+
+            /*
+            Random rnd = new Random(9);
+            double[] dizi = new double[10];
+            double[]dizi1= new double[10];
+            for (int i = 0; i < dizi.Length; i++) {
+                dizi[i] = rnd.Next(1, 9);
+                Console.Write(dizi[i]+" ");
+            }
+            Console.WriteLine();
+            for (int i = 0; i < dizi1.Length; i++)
+            {
+                dizi1[i] = dizi[i] + (dizi[i] * 10) / 100;
+                Console.Write(dizi1[i] + " ");
+            }
+            */
+
+            //5- N x M uzunluklu matris oluşturunuz n m kullanıcıdan alınıcaktır.
+            //elemanların herbiri 3 olsun.
+
+            /*
+            int n, m;
+            Console.WriteLine("Satır sayısını giriniz:");
+            n = int.Parse(Console.ReadLine());
+            Console.WriteLine("Sütun sayısını giriniz:");
+            m = int.Parse(Console.ReadLine());
+            int[,]matris=new int[n,m];
+            for (int i = 0; i < n; i++)
+            {
+                for(int j = 0; j < m; j++)
+                {
+                    matris[i,j] = 3;
+                    Console.Write(matris[i,j]+" ");
+                }
+                Console.WriteLine();
+            }
+            */
+
+            //6- 2x3 uzunluklu matris oluşturunuz elemanları 4 olsun matrisyel görünüme sahip olsun
+
+            /*
+            int top = 0;
+            int[,] matris = new int[2, 3];
+            Console.WriteLine(matris.Length); //C# :2x3=6
+            Console.WriteLine(matris.GetLength(0)); //satır sayısı
+            Console.WriteLine(matris.GetLength(1)); //sutun sayisi
+
+            for (int i = 0; i < matris.GetLength(0); i++)
+            {
+                for (int j = 0; j < matris.GetLength(1); j++)
+                {
+                    matris[i, j] = 4;
+                    Console.Write(matris[i, j] + " ");
+                    top += matris[i, j];
+                }
+                Console.WriteLine();
+            }
+            Console.WriteLine(top);
+            */
+
+            //7- 3x3 rastgele sayılardan oluşan matris dizisi oluşturunuz.random sayılar ile
+            //oluşsun 1. diziyi 5 ile genişleterek   2. diziyi oluşturunuz
+
+            /*
+            int top = 0,n;
+            int[,] matris= new int[3,3];
+            int[,] matris1 = new int[3, 3];
+            n = Convert.ToInt32(Console.ReadLine());
+            Random nsn= new Random();
+            for (int i = 0; i < matris.GetLength(0); i++)
+            {
+                for (int j = 0; j < matris.GetLength(1); j++)
+                {
+                    matris[i, j] = nsn.Next(1,10);
+                    Console.Write(matris[i, j] + " ");
+                    matris1[i, j] = matris[i, j] * n;
+                }
+                Console.WriteLine();
+            }
+            Console.WriteLine();
+            for (int i = 0; i < matris1.GetLength(0); i++)
+            {
+                for (int j = 0; j < matris1.GetLength(1); j++)
+                {
+                    Console.Write(matris1[i,j]+" ");
+                }
+                Console.WriteLine();
+            }
+            */
+
+            //MATRİS GELİR DİZİ GELİR VİZE DE DAHA AĞIRLIKLIDIR.
+
+            //3- 9 elemanlı bir sayı dizisi tanımlayınız.dizinin elemanlarını kullanıcan alarak diziyi oluşturunuz.yan yana yazdırınız ilk ortanca
+            //ve sonuncu elemanlarını toplayınız.
+            
+            int top;
+            int[] dizi = new int[10];
+            for (int i = 0; i < dizi.Length; i++)
+            {
+                Console.WriteLine("Dizinin "+(i+1)+". Elemanını giriniz:");
+                dizi[i] = Convert.ToInt32(Console.ReadLine());
+            }
+            for (int i = 0;i < dizi.Length; i++) 
+            {
+                Console.Write(dizi[i]+" ");
+            }
+            top = dizi[0] + dizi.Length-1 + dizi[dizi.Length / 2];
+            Console.WriteLine(" ");
+            Console.WriteLine(top);
+            
+
+            Console.ReadLine();
+        }
+    }
+}
